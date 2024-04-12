8dd6fba6294124ffcfca06b8143ad16d0e0ba178
Sergey Poznyakoff <gray@gnu.org>
2024-04-12 08:42:31 +0200

    Remove some leftover files

--- dicoweb/conf/docker/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export DJANGO_SETTINGS_MODULE=run.settings_docker
-
-python manage.py collectstatic --no-input
-chgrp -R users /app/static/ && chmod -R g+w /app/static
-
-/usr/local/bin/supervisord -c /etc/supervisord.conf
